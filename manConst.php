306e8e6f07db82247b68d3c7de56b58147b7f11e
Whytmor <makedonskija297@gmail.com>
2023-08-24 19:33:55 +0300

    feat: lesson 5 Combined html and php mode

--- manConst.php
+++ manConst.php
@@ -35,3 +35,28 @@ elseif ($a < 0) :
 else : 
     echo "Переменная а равна нулю";
 endif;
+?>
+
+//Комбинированный режим html и php
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <meta charset='utf-8' />
+</head>
+<body>
+<?php
+$a = -5;
+?>
+
+<?php if ($a > 0) { ?>
+    <h2>Переменная а больше нуля</h2>
+<?php } elseif ($a < 0) { ?>
+    <h2>Переменная а меньше нуля</h2>
+<?php } else { ?>
+    <h2>Переменная а равна нулю</h2>
+<?php } ?>
+</body>
+</html>
+
+
