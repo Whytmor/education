dd48056fd013770676cc73b71d20e05f5500b895
Whytmor <makedonskija297@gmail.com>
2023-08-24 16:00:52 +0300

    lesson_1.2

--- hello.php
+++ hello.php
@@ -5,9 +5,16 @@
 <meta charset='utf-8' />
 </head>
 <body>
-    <h1> Сайт на PHP </h1>
-<?php 
-echo 'Привет Мир!';
+<h1>
+<?php
+echo 'Первый сайт на PHP';
 ?>
+</h1>
+<div>
+<?php
+echo "<h2> Заголовок параграфа </h2>";
+echo 'Текст параграфа';
+?>
+</div>
 </body>
 </html>
