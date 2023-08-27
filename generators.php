64f99ed2bf02522d46488c4b192c741280ecc5df
Whytmor <makedonskija297@gmail.com>
2023-08-28 00:28:30 +0300

    feat: getting to know the generator yield + from operator

--- generators.php
+++ generators.php
@@ -31,4 +31,32 @@ foreach (generateNumbers1() as $index => $number) {
     echo "{$index} - $number<br />"; // 012345
 }
 
+//оператор yield + from
+function generateNumbers()
+{
+    yield 1;
+    yield from [2, 3, 4];
+    yield 5;
+}
+
+foreach (generateNumbers() as $number) {
+    echo $number . "<br />"; // 12345
+}
+
+function generateNumberss($start, $end)
+{
+    for ($i = $start; $i < $end; $i++) {
+        yield $i;
+    }
+}
+foreach (generateNumberss(4, 9) as $number) {
+    echo $number . '<br />';
+}
+
+// перебор массива обычным способом - с большими данными затратно!
+$numbers = [1, 2, 3, 4, 5];
+foreach ($numbers as $number) {
+    echo $number; // 12345
+}
+
 
