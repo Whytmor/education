105c3063447dd0addb2a694103fdb38a271bb4fc
Whytmor <makedonskija297@gmail.com>
2023-08-27 23:58:14 +0300

    feat: Closure

--- closure.php
+++ closure.php
@@ -7,3 +7,14 @@ $showNumber = function () use ($number)
     echo $number;
 };
 $showNumber();
+
+$a = 8;
+$b = 10;
+
+$closure = function ($c) use ($a, $b)
+{
+    return $a + $b + $c;
+};
+
+$result = $closure(22); // 40
+echo $result;
