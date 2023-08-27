a3bea47f2fcfe93b1708063a58ef8c87c59f0e17
Whytmor <makedonskija297@gmail.com>
2023-08-27 12:36:56 +0300

    feat: Variable number of parameters

--- paramFunctions.php
+++ paramFunctions.php
@@ -39,3 +39,42 @@ function displayInfo2($name, $age = 18)
 displayInfo2(age: 23, name: "Bob");
 displayInfo2(name: 'Tom', age: 36);
 displayInfo2(name: 'Alice');
+
+// Переменное количество параметров
+echo '<br>';
+function sum(...$numbers)
+{
+    $result = 0;
+    foreach ($numbers as $number) {
+        $result += $number;
+    }
+    echo "<p>Сумма: {$result}</p>";
+}
+
+sum(1, 2, 3);
+sum(2, 3);
+sum(4, 5, 8, 10);
+
+function sum1(...$numbers)
+{
+    $result = 0;
+    foreach ($numbers as $number) {
+        $result += $number;
+    }
+    echo "<p>Сумма: {$result}</p>";
+}
+$numbers = [3, 5, 7, 8];
+sum1(...$numbers); //23
+
+function getAverageScore($name, ...$scores) 
+{
+    $result = 0.0;
+    foreach ($scores as $score) {
+        $result += $score;
+    }
+    $result = $result /count($scores);
+    echo "<p>{$name}: {$result}</p>";
+}
+getAverageScore('Tom', 5, 5, 4, 5);
+getAverageScore('bob', 4, 3, 4, 4, 4);
+
