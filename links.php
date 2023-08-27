57846c933e0ee8aa683e62cf870957eb08162277
Whytmor <makedonskija297@gmail.com>
2023-08-28 00:48:34 +0300

    feat: Returning a link from a function

--- links.php
+++ links.php
@@ -16,6 +16,7 @@ $sam = &$tom;
 $sam = & $tom;
 
 //Передача по ссылке
+// стандартная передача параметров без ссылки
 function square($a)
 {
     $a *= $a;
@@ -25,7 +26,29 @@ $number = 5;
 square($number);
 echo "<br /> number = {$number}";
 
+// Передача параметра по ссылке
+function square1(&$a)
+{
+    $a *= $a;
+    echo "a = {$a}"; // a = 25 number = 5
+}
 
+$number = 5;
+square1($number);
+echo "<br /> number = {$number}"; // a = 25 number = 25
+
+//Возвращение ссылки из функции
+function &checkName(&$name)
+{
+    if ($name === 'admin') {
+        $name = 'Tom';
+    }
+    return $name;
+}
+$userName = 'admin';
+$checkedName = &checkName($userName);
+echo "<br /> userName: {$userName}";
+echo "<br /> checkedName: {$checkedName}";
 
 
 
