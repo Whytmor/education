9316751ae9b500b236c3c57dfe3f4dcf205acdd8
Whytmor <makedonskija297@gmail.com>
2023-08-27 11:45:12 +0300

    feat: Iterating through an associative multidimensional array

--- multidimensionalArrays.php
+++ multidimensionalArrays.php
@@ -31,5 +31,44 @@ echo $families[1][1] . '<br /'; // kate
             }
             ?>
         </table>
+
+        <br>
+
+        <?php 
+        $phones = array(
+                'apple' => array ('Iphone 12', 'Iphone X', 'Iphone 12 Pro'),
+                'samsung' => array ('Samsung Galaxy s20', 'Samsung Galaxy s20 Ultra'),
+                'nokia' => array ('Nokia 83', 'Nokia 34')
+        );
+        foreach ($phones as $brand => $items) {
+            echo "<h3>{$brand}</h3>";
+            echo "<ul>";
+            foreach ($items as $key => $value) {
+                echo "<li>$value</li>";
+            }
+            echo "</ul>";
+        }
+        ?>
+
+        <br>
+
+        <?php
+        $gadgets = [
+            'phones' => ['apple' => 'Iphone 12', 
+                        'samsung' => 'Samsung S20', 
+                        'nokia' => 'Nokia 83'],
+            'tablets' => ['lenovo' => 'Lenovo Toga Smart Tab', 
+                        'samsung' => 'Samsung Galaxy Tab s5', 
+                        'apple' => 'Apple Ipad Pro']
+        ];
+        foreach ($gadgets as $gadget => $items) {
+            echo "<h3>{$gadget}</h3>";
+            echo "<ul>";
+            foreach ($items as $key => $value) {
+                echo "<li> {$key} : {$value} </li>";
+            }
+            echo "</ul>";
+        }
+        ?>
     </body>
 </html>
