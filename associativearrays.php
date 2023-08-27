be7269dd001c31ff9c51c229c572280ece982703
Whytmor <makedonskija297@gmail.com>
2023-08-27 10:43:34 +0300

    feat: associative arrays

---
+++ associativearrays.php
@@ -0,0 +1,21 @@
+<?php
+$words = array ("red" => "красный", "blue" => "синий", "green" => "зелёный");
+
+$words = ["red" => "красный", "blue" => "синий", "green" => "зелёный"];
+
+$countries = ["Germany" => "Berlin", "France" => "Paris", "Spain" => "Madrid"];
+echo $countries["Spain"]; // Madrid
+echo '<br />';
+
+$countries["Spain"] = "Barcelona";
+echo $countries["Spain"]; // Barcelona
+
+$countries = ["Germany" => "Berlin", "France" => "Paris", "Spain" => "Madrid"];
+$countries["Italy"] = "Rome"; // определяем новый элемент с ключом Italy
+echo $countries["Italy"]; // Rome
+
+$countries["Germany"] = "Berlin";
+$countries["France"] = "Paris";
+$countries["Spain"] = "Madrid";
+$countries["Italy"] = "Rome";
+echo $countries["Italy"]; // Rome
