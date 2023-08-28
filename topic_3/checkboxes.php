1397eaa9da448289475bc5aec3992b9c647db114
Whytmor <makedonskija297@gmail.com>
2023-08-28 15:36:29 +0300

    feat: list selection of 1 item

--- topic_3/checkboxes.php
+++ topic_3/checkboxes.php
@@ -26,16 +26,30 @@ if (isset($_POST['remember'])) {
             $course = $_POST['course'];
             echo $course;
         }
+        if (isset($_POST['list'])) {
+            $list = $_POST['list'];
+            echo $list;
+        }
         ?>
         <h3>Форма ввода данных</h3>
         <form method="post">
+            <h4>Флажки</h4>
             <p>ASP.NET: <input type="checkbox" name="technologies[]" value="ASP.NET" /></p>
             <p> PHP: <input type="checkbox" name="technologies[]" value="PHP" /></p>
             <p>Node.js: <input type="checkbox" name="technologies[]" value="Node.js" /></p>
+            <h4>Переключатели</h4>
             <input type="radio" name="course" value="ASP.NET" /> ASP.NET <br>
             <input type="radio" name="course" value="PHP" /> PHP <br>
             <input type="radio" name="course" value="Node.js" /> Node.js <br> 
+            <h4>Список</h4>
+            <select name="list" size="1">
+                <option value="ASP.NET">ASP.NET</option>
+                <option value="PHP">PHP</option>
+                <option value="Ruby">Ruby</option>
+                <option value="Python"> Python </option>
+            </select>
             <input type="submit" value="отправить">
+            
         </form>
     </body>
 </html>
