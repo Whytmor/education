2cc055420c4742cb3a26a7dacfc5b9b08fd08abb
Whytmor <makedonskija297@gmail.com>
2023-08-28 14:31:58 +0300

    feat: Explicit indication of the users array keys

--- topic_3/users.php
+++ topic_3/users.php
@@ -18,19 +18,27 @@ foreach ($users as $user) {
     <body>
         <?php
         if (isset($_POST['users'])) {
-            $users = $_POST['users'];
-            echo "В массиве " . count($users) . " элемента/ов<br />";
-            foreach ($users as $user) {
-                echo "{$user}<br />";
-            }
+            // $users = $_POST['users'];
+            // echo "В массиве " . count($users) . " элемента/ов<br />";
+            // foreach ($users as $user) {
+            //     echo "{$user}<br />";
+            // }
+            $firstUser = $_POST['users']['first'];
+            $secondUser = $_POST['users']['second'];
+            $thirdUser = $_POST['users']['third'];
+            echo "{$firstUser}<br>{$secondUser}<br>{$thirdUser}";
+            print_r($_POST['users']);
         }
         
         ?>
         <h3>Форма ввода данных</h3>
         <form method="POST">
-            <p>User 1: <input type="text" name="users[]" /></p>
+            <!-- <p>User 1: <input type="text" name="users[]" /></p>
             <p>User 2: <input type="text" name="users[]" /></p>
-            <p>User 3: <input type="text" name="users[]" /></p>
+            <p>User 3: <input type="text" name="users[]" /></p> -->
+            <p>User 1: <input type="text" name="users[first]"></p>
+            <p>User 2: <input type="text" name="users[second]"></p>
+            <p>User 3: <input type="text" name="users[third]"></p>
             <input type='submit' value='отправить'>
         </form>
     </body>
