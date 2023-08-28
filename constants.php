ec0bbe3529d27b9f13571032ed8629f575e8277c
Whytmor <makedonskija297@gmail.com>
2023-08-28 09:44:56 +0300

    feat: Checking the existence of a constant

--- constants.php
+++ constants.php
@@ -7,6 +7,14 @@ echo PI; //3.1516
 define('NUMBER', 22);
 echo NUMBER; // 22
 
+
 echo " <br>Строка " . __LINE__ . " в файле" . __FILE__;
 
+// Проверка существования константы
+const PI1 = 3.14;
+if (!defined('PI1')) {
+    define('PI1', 3.14);
+} else {
+    echo 'Константа PI1 уже определена!';
+}
 
