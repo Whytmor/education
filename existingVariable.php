4edf1f889467f1ba12106af625aeeb390b16d8b3
Whytmor <makedonskija297@gmail.com>
2023-08-28 09:54:29 +0300

    feat: function isset and null value

--- existingVariable.php
+++ existingVariable.php
@@ -2,3 +2,25 @@
 $a; 
 echo $a;
 
+//оператор isset
+$message;
+if (isset($message)) {
+    echo $message;
+} else {
+    echo 'переменная message не определена';
+}
+
+$message = 'Hello PHP';
+if (isset($message)) {
+    echo $message;
+} else {
+    echo 'переменная message не определена';
+}
+
+// значение null isset вернёт false
+$message = null;
+if (isset($message)) {
+    echo $message;
+} else {
+    echo 'переменная не определена';
+}
