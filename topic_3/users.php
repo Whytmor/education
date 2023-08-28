c263f0c1fece20d0ec4a8812cd4d8a0fa9917cef
Whytmor <makedonskija297@gmail.com>
2023-08-28 14:20:33 +0300

    feat: sending data in a POST request

--- topic_3/users.php
+++ topic_3/users.php
@@ -9,3 +9,29 @@ foreach ($users as $user) {
 }
 
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta charset='utf8'>
+    </head>
+    <body>
+        <?php
+        if (isset($_POST['users'])) {
+            $users = $_POST['users'];
+            echo "В массиве " . count($users) . " элемента/ов<br />";
+            foreach ($users as $user) {
+                echo "{$user}<br />";
+            }
+        }
+        
+        ?>
+        <h3>Форма ввода данных</h3>
+        <form method="POST">
+            <p>User 1: <input type="text" name="users[]" /></p>
+            <p>User 2: <input type="text" name="users[]" /></p>
+            <p>User 3: <input type="text" name="users[]" /></p>
+            <input type='submit' value='отправить'>
+        </form>
+    </body>
+</html>
