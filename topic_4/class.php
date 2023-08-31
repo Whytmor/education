092313ba821bf3567970e6cd638921718e114bbc
Whytmor <makedonskija297@gmail.com>
2023-08-31 07:46:39 +0300

    feat: properties and methods of the class

--- topic_4/class.php
+++ topic_4/class.php
@@ -5,3 +5,37 @@ class Person
 }
 $person = new Person();
 print_r($person);
+
+class Person1
+{
+    public $name, $age;
+
+    function hello() 
+    {
+        echo 'Hello!<br>';
+    }
+}
+$tom = new Person1();
+$tom->name = 'tom'; // установка свойства $name
+$tom->age = 36; // Установка свойства $age
+$personName = $tom->name; //получение значение свойства $name
+echo "Имя пользователя: {$personName} <br>";
+$tom->hello(); // вызов метода hello()
+print_r($tom);
+
+class Person3
+{
+    public $name = 'Undefined', $age = 18;
+
+    function hello()
+    {
+        echo 'hello<br>';
+    }
+}
+$tom = new Person3();
+$tom->age = 36; // установка свойства age
+echo "Имя пользователя: {$tom->name}<br>";
+echo "Возраст пользователя: {$tom->age}<br>";
+print_r($tom);
+
+
