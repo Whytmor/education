51848f316a146b0758b9dd08f7977e161ea9e572
Whytmor <makedonskija297@gmail.com>
2023-08-31 09:26:41 +0300

    feat: Redefining functionality

--- topic_4/Inheritance.php
+++ topic_4/Inheritance.php
@@ -1,6 +1,7 @@
 <?php
 
 use Person as GlobalPerson;
+use Person1 as GlobalPerson1;
 
 class Person
 {
@@ -19,4 +20,35 @@ class Employee extends GlobalPerson
 
 }
 $tom = new Employee('Tom');
+$tom -> displayInfo();
+
+//Переопределение функционала
+class Person1
+{
+    public $name;
+    function __construct($name)
+    {
+        $this -> name = $name;
+    }
+    function displayInfo() 
+    {
+        echo "Имя: {$this -> name}";
+    }
+}
+class Employee1 extends GlobalPerson1
+{
+    public $company;
+    function __construct($name, $company)
+    {
+        $this -> name = $name;
+        $this -> company = $company;
+    }
+    function displayInfo()
+    {
+        echo "Имя: {$this -> name}<br>";
+        echo "Работает в {$this -> company} <br>";
+    }
+}
+
+$tom = new Employee1('Tom', 'Microsoft');
 $tom -> displayInfo();
