cbd578d4a4a7dbdd7f2592a04e80d32ed6c980f0
Whytmor <makedonskija297@gmail.com>
2023-08-31 08:08:48 +0300

    feat: Comparison of class objects

--- topic_4/class.php
+++ topic_4/class.php
@@ -69,3 +69,49 @@ $bob = new Person4();
 $bob->name = 'Bob';
 $bob->age = 25;
 $bob->displayInfo();
+
+class Person5
+{
+    public $name, $age;
+    function displayInfo()
+    {
+        echo "Name: {$this->name}; Age: {$this->age}";
+    }
+}
+$tom = new Person5();
+$tom -> name = 'Tom';
+$tom -> age = 3;
+
+$tomas = new Person5();
+$tomas -> name = 'Tom';
+$tomas -> age = 36;
+
+if ($tom == $tomas) {
+    echo 'Переменные tom и tomas равны<br>';
+} else {
+    echo 'Переменные tom и tomas не равны<br>';
+}
+
+if ($tom === $tomas) {
+    echo 'Переменные tom и tomas эквивалентны<br>';
+} else {
+    echo 'Переменные tom и tomas не эквивалентны<br>';
+}
+
+$person = new Person5();
+$tom = $person;
+$tom -> name = 'Tom';
+$tom -> age = 36;
+
+$tomas = $person;
+if ($tom == $tomas) {
+    echo 'Переменные tom and tomas равны<br>';
+} else {
+    echo 'Переменные tom and tomas не равны <br>';
+}
+
+if ($tom === $tomas) {
+    echo 'Переменные tom and tomas эквивалентны';
+} else {
+    echo 'Переменные tom and tomas не эквивалентны';
+}
