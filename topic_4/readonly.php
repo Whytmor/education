929302307de5a4725384519e94cb5f0187abeb1e
Whytmor <makedonskija297@gmail.com>
2023-08-31 13:53:11 +0300

    feat: Classes for reading

--- topic_4/readonly.php
+++ topic_4/readonly.php
@@ -30,3 +30,38 @@ $tom = new Person2();
 $tom -> init(); // инициализация свойства name
 echo "Name: {$tom->name}"; //name bob
 
+// Классы для чтения
+readonly class Person4
+{
+    public string $name;
+    public int $age;
+    public function __construct($name, $age)
+    {
+        $this -> name = $name;
+        $this -> age = $age;
+    }
+}
+
+//эквивалентно
+class Person5
+{
+    public readonly string $name;
+    public readonly int $age;
+    public function __construct($name, $age)
+    {
+        $this -> name = $name;
+        $this -> age = $age;
+    }
+}
+
+readonly class Person6
+{
+    public function __construct(public string $name, public int $age)
+    {}
+}
+
+$tom = new Person6('Tom', 38);
+//Получить значения свойств можно
+echo "Name: {$tom -> name} Age: {$tom -> age}"// name Tom Age 38
+// изменить значения свойств нелья
+//$tom -> name = "Tomas"; // Ошибка!
