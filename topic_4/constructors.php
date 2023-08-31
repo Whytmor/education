22b807ea4c852a4896f10941bfcf9bbd2da93b09
Whytmor <makedonskija297@gmail.com>
2023-08-31 08:28:12 +0300

    feat: Default Settings

--- topic_4/constructors.php
+++ topic_4/constructors.php
@@ -18,3 +18,26 @@ $tom ->displayInfo();
 
 $bob = new Person('Bob', 41);
 $bob->displayInfo();
+
+class Person2 
+{
+    public $name, $age;
+    function __construct($name = 'Tom', $age = 36)
+    {
+        $this -> name = $name;
+        $this -> age = $age;
+    }
+    function displayInfo()
+    {
+        echo "Name: {$this->name}; Age: {$this->age}<br>";
+    }
+}
+$tom = new Person2();
+$tom->displayInfo();
+
+$bob = new Person2();
+$bob -> displayInfo('Bob');
+
+$sam = new Person('Sam', 29);
+$sam ->displayInfo();
+
