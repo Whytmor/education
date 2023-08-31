290f4df199a3dafefc7f589d49f60f90ebfbfa9b
Whytmor <makedonskija297@gmail.com>
2023-08-31 09:51:39 +0300

    feat: Instanceof operator

--- topic_4/Inheritance.php
+++ topic_4/Inheritance.php
@@ -3,6 +3,7 @@
 use Person as GlobalPerson;
 use Person1 as GlobalPerson1;
 use Person2 as GlobalPerson2;
+use Person3 as GlobalPerson3;
 
 class Person
 {
@@ -106,3 +107,39 @@ class Employee3 extends Person2
 }
 $tom1 = new Employee3('Tom1', 'Microsoft1');
 $tom1 -> displayInfo();
+
+
+class Person3
+{
+    public $name;
+    function __construct($name)
+    {
+        $this -> name = $name;
+    }
+    function displayInfo()
+    {
+        echo "Имя: {$this -> name}<br>";
+    }
+}
+class Employee4 extends Person3
+{
+    public $company;
+    function __construct($name, $company)
+    {
+        Person3::__construct($name);
+        $this -> company = $company;
+    }
+    function displayInfo()
+    {
+        Person3::displayInfo();
+        echo "Работает в {$this -> company}<br>";
+    }
+}
+class Manager
+{}
+
+$tom = new Employee4('Tom', 'Microsoft');
+
+$tom instanceof Employee4; //true
+$tom instanceof Person3; // true
+$tom instanceof Manager; // false
